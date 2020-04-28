b96083a52c0aad2bca3e466be9c87e715a843f26
Rodrigo Viola <rv.rodrigo.viola@gmail.com>
2020-04-28 17:11:23 +0200

    a v0.4

--- js/lang.js
+++ js/lang.js
@@ -1,7 +1,7 @@
 
 const langEs = {
   lang: "es",
-  opLang: "ES",
+  opLang: "EN",
   title: "Agencia Marketing Digital | Ranking Online",
   navBar: {
       home: "Bienvenido",
@@ -13,46 +13,46 @@ const langEs = {
   },
   services:{
     serv1:{
-        title: "Diseño Web",
-        text1: "",
-        text2: "",
-        text3: ""
+        title: "Web Design",
+        text1: "Ranking Online te ofrece el servicio de diseño web que mejor se adapte a tu marca, a través de una navegación atractiva rápida e intuitiva. Digitalizamos tu negocio para poder llegar así un mayor número de clientes.",
+        text2: "Somos capaces de llevar a Internet tu negocio local, respetando la esencia que le caracteriza.",
+        text3: "Somos expertos en crear páginas webs adaptadas a versión móvil y ordenador, contando con las últimas tendencias en posicionamiento web."
     },
     serv2:{
         title: "SEO",
-        text1: "",
-        text2: "",
-        text3: ""
+        text1: "En <strong>Ranking Online</strong> te ayudamos a posicionar tu página web a través de los motores de búsqueda. El Posicionamiento SEO es la herramienta más recomendable para tener una buena presencia en internet, ya que a medio y largo plazo será más rentable y económica.",
+        text2: "Obtener el primer resultado de búsqueda en Internet, equivale a ocupar el local físico mejor posicionado en el centro de la ciudad.",
+        text3: "Conocemos cuáles son las técnicas de posicionamiento que tanto gusta a Google y que pondremos en marcha adaptándolas a tu negocio."
     },
     serv3:{
-        title: "Producción Audiovisual",
-        text1: "",
-        text2: "",
-        text3: ""
+        title: "Multimedia Production",
+        text1: "En Ranking Online te ayudamos a posicionar tu página web a través de los motores de búsqueda. El Posicionamiento SEO es la herramienta más recomendable para tener una buena presencia en internet, ya que a medio y largo plazo será más rentable y económica.",
+        text2: "Obtener el primer resultado de búsqueda en Internet, equivale a ocupar el local físico mejor posicionado en el centro de la ciudad.",
+        text3: "Conocemos cuáles son las técnicas de posicionamiento que tanto gusta a Google y que pondremos en marcha adaptándolas a tu negocio."
     },
     serv4:{
         title: "Branding",
-        text1: "",
-        text2: "",
-        text3: ""
+        text1: "Branding no es solamente la creación de un logo, sino que agrupa todas aquellas acciones encargadas de construir y gestionar la imagen de tu marca.",
+        text2: "Una marca que llega a la mente, produce una reacción. Una marca que llega al corazón, genera un compromiso.",
+        text3: "Nos encargamos de dar vida a tu marca, atribuyéndole una serie de valores, llegando así a tus consumidores y creando una imagen identificativa y diferenciativa frente a tu competencia."
     },
     serv5:{
         title: "Google My Business",
-        text1: "",
-        text2: "",
-        text3: ""
+        text1: "Es muy importante que tus clientes potenciales sepan donde encontrarte geográficamente. Ponle fácil a tu usuario el camino hacia tu negocio. ",
+        text2: "Invita a tus clientes potenciales a que descubran las instalaciones de tu negocio, teniendo de esta forma un trato más cercano y personal con ellos.",
+        text3: "En Ranking Online somos expertos en dar visibilidad a tu empresa y gestionar tu presencia física en Internet."
     },
     serv6:{
-        title: "Publicidad (SEM)",
-        text1: "",
-        text2: "",
-        text3: ""
+        title: "Advertising (SEM)",
+        text1: "Te ayudamos a filtrar tu público objetivo y planificar una estrategia SEM adaptada a tu negocio y a tu público. <br> No te pongas límites y llega al mayor número de clientes potenciales, incrementando el número de ventas de tus productos o servicios.",
+        text2: "Gestionamos tus campañas de publicidad en Google Adwords y redes sociales, sacando el máximo partido a tu negocio.",
+        text3: "La publicidad en Google permite recibir usuarios a tu web desde el primer dia."
     },
     serv7:{
-        title: "Redes Sociales",
-        text1: "",
-        text2: "",
-        text3: ""
+        title: "Social Media",
+        text1: "Incluye en tu plan de marketing digital la creación y gestión de redes sociales. Hoy en día, es esencial tener una buena presencia en las redes para estar en continuo contacto con tus clientes y darte a conocer a nuevos clientes.",
+        text2: "Recorta distancias con tus clientes y acércate a ellos gracias a una buena estrategia en redes sociales.",
+        text3: "En Ranking Online nos ocupamos de implementar una adecuada estrategia en tus redes sociales. Reinventa tu marca y súmate a las ventajas que te pueden aportar."
     },
     serv8:{
         title: "serv8.title",
@@ -78,7 +78,7 @@ const langEs = {
 
 const langEn = {
   lang: "en",
-  opLang: "EN",
+  opLang: "ES",
   title: "Digital Marketing Agency | Ranking Online",
   navBar: {
       home: "Bienvenido",
@@ -91,45 +91,45 @@ const langEn = {
   services:{
     serv1:{
         title: "Web Design",
-        text1: "",
-        text2: "",
-        text3: ""
+        text1: "Ranking Online te ofrece el servicio de diseño web que mejor se adapte a tu marca, a través de una navegación atractiva rápida e intuitiva. Digitalizamos tu negocio para poder llegar así un mayor número de clientes.",
+        text2: "Somos capaces de llevar a Internet tu negocio local, respetando la esencia que le caracteriza.",
+        text3: "Somos expertos en crear páginas webs adaptadas a versión móvil y ordenador, contando con las últimas tendencias en posicionamiento web."
     },
     serv2:{
         title: "SEO",
-        text1: "",
-        text2: "",
-        text3: ""
+        text1: "En <strong>Ranking Online</strong> te ayudamos a posicionar tu página web a través de los motores de búsqueda. El Posicionamiento SEO es la herramienta más recomendable para tener una buena presencia en internet, ya que a medio y largo plazo será más rentable y económica.",
+        text2: "Obtener el primer resultado de búsqueda en Internet, equivale a ocupar el local físico mejor posicionado en el centro de la ciudad.",
+        text3: "Conocemos cuáles son las técnicas de posicionamiento que tanto gusta a Google y que pondremos en marcha adaptándolas a tu negocio."
     },
     serv3:{
         title: "Multimedia Production",
-        text1: "",
-        text2: "",
-        text3: ""
+        text1: "En Ranking Online te ayudamos a posicionar tu página web a través de los motores de búsqueda. El Posicionamiento SEO es la herramienta más recomendable para tener una buena presencia en internet, ya que a medio y largo plazo será más rentable y económica.",
+        text2: "Obtener el primer resultado de búsqueda en Internet, equivale a ocupar el local físico mejor posicionado en el centro de la ciudad.",
+        text3: "Conocemos cuáles son las técnicas de posicionamiento que tanto gusta a Google y que pondremos en marcha adaptándolas a tu negocio."
     },
     serv4:{
         title: "Branding",
-        text1: "",
-        text2: "",
-        text3: ""
+        text1: "Branding no es solamente la creación de un logo, sino que agrupa todas aquellas acciones encargadas de construir y gestionar la imagen de tu marca.",
+        text2: "Una marca que llega a la mente, produce una reacción. Una marca que llega al corazón, genera un compromiso.",
+        text3: "Nos encargamos de dar vida a tu marca, atribuyéndole una serie de valores, llegando así a tus consumidores y creando una imagen identificativa y diferenciativa frente a tu competencia."
     },
     serv5:{
         title: "Google My Business",
-        text1: "",
-        text2: "",
-        text3: ""
+        text1: "Es muy importante que tus clientes potenciales sepan donde encontrarte geográficamente. Ponle fácil a tu usuario el camino hacia tu negocio. ",
+        text2: "Invita a tus clientes potenciales a que descubran las instalaciones de tu negocio, teniendo de esta forma un trato más cercano y personal con ellos.",
+        text3: "En Ranking Online somos expertos en dar visibilidad a tu empresa y gestionar tu presencia física en Internet."
     },
     serv6:{
         title: "Advertising (SEM)",
-        text1: "",
-        text2: "",
-        text3: ""
+        text1: "Te ayudamos a filtrar tu público objetivo y planificar una estrategia SEM adaptada a tu negocio y a tu público. <br> No te pongas límites y llega al mayor número de clientes potenciales, incrementando el número de ventas de tus productos o servicios.",
+        text2: "Gestionamos tus campañas de publicidad en Google Adwords y redes sociales, sacando el máximo partido a tu negocio.",
+        text3: "La publicidad en Google permite recibir usuarios a tu web desde el primer dia."
     },
     serv7:{
         title: "Social Media",
-        text1: "",
-        text2: "",
-        text3: ""
+        text1: "Incluye en tu plan de marketing digital la creación y gestión de redes sociales. Hoy en día, es esencial tener una buena presencia en las redes para estar en continuo contacto con tus clientes y darte a conocer a nuevos clientes.",
+        text2: "Recorta distancias con tus clientes y acércate a ellos gracias a una buena estrategia en redes sociales.",
+        text3: "En Ranking Online nos ocupamos de implementar una adecuada estrategia en tus redes sociales. Reinventa tu marca y súmate a las ventajas que te pueden aportar."
     },
     serv8:{
         title: "serv8.title",
